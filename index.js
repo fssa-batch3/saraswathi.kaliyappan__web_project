3c53424b49e46da1fd2c5b77f7242e7ef9131197
saraswathi234 <saraswathisaraswathi81330@gmail.com>
2023-01-11 15:09:05 +0530

    path

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ for(let count=0; count<100; count++){
         console.log ( count+ "Superstar")
         console.log (count+ "rajini")
 }
-for(let count=0; count<100; count++ );
-if(superstar==="superstar")
+/let string='';
+// let columns = 5;
+// let rows = 5;
+// for (let i = 1; i <= rows; i++) {
+//  for (let j = 1; j <= columns; j++) {
+//    string =string + i + ' ';
+//  }
+//  string += '\n';
+// }
+// console.log(string);
+
+const n=7;
 
 
 
--- saraswathi.js
+++ saraswathi.js
@@ -3,3 +3,22 @@ for(let count=0; count<50; count++){
     console.log(Count+ "ramya")
 
 }
+let string='7';
+// let columns = 5;
+// let rows = 5;
+// for (let i = 1; i <= rows; i++) {
+//  for (let j = 1; j <= columns; j++) {
+//    string =string + i + ' ';
+//  }
+//  string += '\n';
+// }
+// console.log(string);
+
+const n=7;
+for(a=1; a<=7; a++){ 
+   let output="7";
+     for(let b=1; b<=7; b++){ 
+        output +=a+"7";
+     }
+}
+console.log="output"
