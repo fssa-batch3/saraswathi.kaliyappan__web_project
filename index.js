e8b6aebc6244450d3d034d060dcfd60d42a3a125
saraswathi234 <saraswathisaraswathi81330@gmail.com>
2023-01-11 14:21:03 +0530

    path

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ for(let count=0; count<100; count++){
         console.log ( count+ "Superstar")
         console.log (count+ "rajini")
 }
+for(let count=0; count<100; count++ );
+if(superstar==="superstar")
+
 
 
 
