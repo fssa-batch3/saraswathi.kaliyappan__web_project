5269fcc23b4a9b116a14272b4fda94ad470c829b
saraswathi234 <saraswathisaraswathi81330@gmail.com>
2023-01-12 13:31:42 +0530

    path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ for(let count=0; count<100; count++){
         console.log ( count+ "Superstar")
         console.log (count+ "rajini")
 }
-/let string='';
+/ let string='7';
 // let columns = 5;
 // let rows = 5;
 // for (let i = 1; i <= rows; i++) {
@@ -12,9 +12,3 @@ for(let count=0; count<100; count++){
 //  string += '\n';
 // }
 // console.log(string);
-
-const n=7;
-
-
-
-
--- saraswathi.js
+++ saraswathi.js
@@ -3,16 +3,6 @@ for(let count=0; count<50; count++){
     console.log(Count+ "ramya")
 
 }
-let string='7';
-// let columns = 5;
-// let rows = 5;
-// for (let i = 1; i <= rows; i++) {
-//  for (let j = 1; j <= columns; j++) {
-//    string =string + i + ' ';
-//  }
-//  string += '\n';
-// }
-// console.log(string);
 
 const n=7;
 for(a=1; a<=7; a++){ 
