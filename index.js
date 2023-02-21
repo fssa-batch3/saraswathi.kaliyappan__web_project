1d6132cc7f0c0863fbf9490076686f422315ea4f
saraswathi234 <saraswathisaraswathi81330@gmail.com>
2023-02-21 10:22:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-for(let count=0; count<100; count++){
-        console.log ( count+ "Superstar")
-        console.log (count+ "rajini")
-}
-/ let string='7';
+// for(let count=0; count<100; count++){
+//         console.log ( count+ "Superstar")
+//         console.log (count+ "rajini")
+// }
+
+//  let string='7';
 // let columns = 5;
 // let rows = 5;
 // for (let i = 1; i <= rows; i++) {
